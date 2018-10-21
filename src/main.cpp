456076c566b7725697f567f362cf5975890ca57d
dipakgmx <shetty.dipak@gmx.com>
2018-10-21 20:37:22 +0200

    Updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "dht.h"
+#include <wiringPi.h>
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    dht DHT11;
+    std::tuple<float, float> dhtValues;
+    std::cout << "Raspberry Pi wiringPi DHT11 Temperature test program" << std::endl;
+
+    if ( wiringPiSetup() == -1 )
+        exit( 1 );
+
+    while ( 1 )
+    {
+        dhtValues = DHT11.getDhtValues();
+        std::cout << "The temperature is " << std::get<0>(dhtValues) << " and humidity is " << std::get<1>(dhtValues) << std::endl;
+        delay( 100 );
+    }
     return 0;
 }
