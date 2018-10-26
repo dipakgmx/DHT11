f882002914fc28c5c844b8d9e3887911f9356779
dipakgmx <shetty.dipak@gmx.com>
2018-10-26 22:24:55 +0200

    Sqlite connections

--- src/dht.cpp
+++ src/dht.cpp
@@ -11,54 +11,59 @@ dht::dht(uint8_t dhtPin, uint8_t timeOut)
 }
 std::tuple<int, int> dht::getDhtValues()
 {
-    std::array<int, 5> dht_values{0};
-    uint8_t lastState	= HIGH;
-    uint8_t counter		= 0;
-    uint8_t j		= 0, i;
+    int temperature{0};
+    int humidity{0};
+    for (int attempt = 0; attempt < 20; attempt++) {
 
-    pinMode( dhtPin, OUTPUT );
-    digitalWrite( dhtPin, LOW );
-    delay( 18 );
-    digitalWrite( dhtPin, HIGH );
-    delayMicroseconds( 40 );
-    pinMode( dhtPin, INPUT );
+        std::array<int, 5> dht_values{0};
+        uint8_t lastState	= HIGH;
+        uint8_t counter		= 0;
+        uint8_t j		= 0, i;
+
+        pinMode( dhtPin, OUTPUT );
+        digitalWrite( dhtPin, LOW );
+        delay( 18 );
+        digitalWrite( dhtPin, HIGH );
+        delayMicroseconds( 40 );
+        pinMode( dhtPin, INPUT );
 
 
-
-    for ( i = 0; i < timeOut; i++ )
-    {
-        counter = 0;
-        while ( digitalRead( dhtPin ) == lastState )
+        for ( i = 0; i < timeOut; i++ )
         {
-            counter++;
-            delayMicroseconds( 1 );
-            if ( counter == 255 )
+            counter = 0;
+            while ( digitalRead( dhtPin ) == lastState )
             {
+                counter++;
+                delayMicroseconds( 1 );
+                if ( counter == 255 )
+                {
+                    break;
+                }
+            }
+
+            lastState = static_cast<uint8_t>(digitalRead(dhtPin ));
+
+            if ( counter == 255 )
                 break;
+
+            if ( (i >= 4) && (i % 2 == 0) )
+            {
+                dht_values[j / 8] <<= 1;
+                if ( counter > 50 )
+                    dht_values[j / 8] |= 1;
+                j++;
             }
         }
 
-        lastState = static_cast<uint8_t>(digitalRead(dhtPin ));
-
-        if ( counter == 255 )
-            break;
-
-        if ( (i >= 4) && (i % 2 == 0) )
+        if ( (j >= 40) &&
+            (dht_values[4] == ( (dht_values[0] + dht_values[1] + dht_values[2] + dht_values[3]) & 0xFF) ) )
         {
-            dht_values[j / 8] <<= 1;
-            if ( counter > 50 )
-                dht_values[j / 8] |= 1;
-            j++;
+            temperature = dht_values[0];
+            humidity = dht_values[2];
+            break;
         }
     }
-
-    if ( (j >= 40) &&
-        (dht_values[4] == ( (dht_values[0] + dht_values[1] + dht_values[2] + dht_values[3]) & 0xFF) ) )
-    {
-        return std::make_tuple(dht_values[0] , dht_values[2]);
-    }else  {
-        return std::make_tuple(-255, -255);
-    }
+    return std::make_tuple(temperature, humidity);
 }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <string>
 #include "dht.h"
+#include "sqlite3.h"
 
-int main()
-{
+int main(int argc, char* argv[]) {
+
+    //Creating database handle -db
+    sqlite3 *db;
+    sqlite3_stmt *res;
+    char *err_msg = nullptr;
+
+    int rc = sqlite3_open("dht11.db", &db);
+
+    if (rc != SQLITE_OK) {
+        std::cerr << "Cannot open database: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        return 1;
+    }
+
+    char *sql = const_cast<char *>("CREATE TABLE IF NOT EXISTS dhtreadings \
+            (id INTEGER PRIMARY KEY , \
+            humidity INTEGER, \
+            temperature INTEGER, \
+            datestamp DEFAULT CURRENT_DATE, \
+            timestamp DEFAULT (time('now', 'localtime'))); \
+    ");
+
+    rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+
+    if (rc != SQLITE_OK) {
+        std::cerr << "Failed to create table!. SQL error " << sqlite3_errmsg(db) << std::endl;
+        return 1;
+    }
+    else {
+        std::cout << "Creation of SQL table successful!" << std::endl;
+    }
+
+
     dht DHT11;
     std::tuple<int, int> dhtValues;
     std::cout << "Raspberry Pi wiringPi DHT11 Temperature test program" << std::endl;
@@ -13,7 +47,22 @@ int main()
     while (true)
     {
         dhtValues = DHT11.getDhtValues();
-        std::cout << "The humidity is " << std::get<0>(dhtValues) << "% and temperature is " << std::get<1>(dhtValues) << " °C"<< std::endl;
+        int humidity = std::get<0>(dhtValues);
+        int temperature = std::get<1>(dhtValues);
+
+        std::string sqlStatement = "INSERT INTO dhtreadings(humidity, temperature) VALUES ("
+                                    +  std::to_string(humidity) +
+                                    "," +  std::to_string(temperature) + ");";
+
+        const char *sqlOperation = sqlStatement.c_str();
+        rc = sqlite3_exec(db, sqlOperation, 0, 0, &err_msg);
+        if (rc != SQLITE_OK) {
+            std::cerr << "Element insertion failed!" << sqlite3_errmsg(db) << std::endl;
+            sqlite3_free(err_msg);
+            sqlite3_close(db);
+            return 1;
+        }
+        std::cout <<  "The humidity is " << std::get<0>(dhtValues) << "% and temperature is " << std::get<1>(dhtValues) << " °C"<< std::endl;
         delay( 2000 );
     }
     return 0;
